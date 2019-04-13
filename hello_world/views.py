31ce9298440516ea26c917202079ad2623c5b2d4
molbinski <molbinski@users.noreply.github.com>
2019-04-13 11:19:01 +0200

    Dockerfile

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "ZMieniłem imie by sprawdzić czy travis zadziałał"
+moje_imie = "ZMieniłem imie"
 msg = "WitajSwiecie!"
 
 
