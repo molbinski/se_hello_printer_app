d826f8327d7c8b8f34a159dea91907d4a9722573
molbinski <molbinski@users.noreply.github.com>
2019-04-13 12:06:15 +0200

    Dockerfile

--- test/test_views.py
+++ test/test_views.py
@@ -14,5 +14,5 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"ZMieniłem imie", "msg":"WitajSwiecie!"}',
+        self.assertEquals('{ "imie":"ZMienilem", "msg":"WitajSwiecie!"}',
                           rv.data)
